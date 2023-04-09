a4ae2e232b2f029e5712fdb57061b6febb5b2073
alexau1012 <alexaumh@gmail.com>
2023-04-09 10:46:35 +1000

    Refactor

--- main.go
+++ main.go
@@ -26,35 +26,30 @@ func main() {
 		dref := fmt.Sprintf("personalisedShowRecommendations/%v/personalisedShows/recommendations", userId)
 
 		var err error
-		if *usecasePtr == "RESET" {
-			err = resetUserRecommmendations(db, cref, userId)
+		switch *usecasePtr {
+		case "RESET":
+			err = db.DeleteCollection(cref, 20)
 			if err != nil {
 				log.Fatalln(err)
 			}
-		}
-
-		if *usecasePtr == "READ_ONLY" {
-			err = readUserRecommendations(db, cref, false, userId)
+		case "READ_ONLY":
+			err = db.ReadCollection(cref, false)
 			if err != nil {
 				log.Fatalln(err)
 			}
-		}
-
-		if *usecasePtr == "READ_WRITE" {
+		case "READ_WRITE":
 			fmt.Printf("Ingesting user <%v> recommendation ids...", userId)
-			err = readUserRecommendations(db, cref, false, userId)
+			err = db.ReadCollection(cref, false)
 			if err != nil {
 				log.Fatalln(err)
 			}
-			err = writeUserRecommendationIds(db, dref,
+			err = db.SetDocument(dref,
 				&domain.Recommendations{Recommendations: config.Recommendations, Meta: config.Meta})
 			if err != nil {
 				log.Fatalln(err)
 			}
-
-			time.Sleep(5 * time.Second)
-
-			err = readUserRecommendations(db, cref, false, userId)
+			time.Sleep(3 * time.Second)
+			err = db.ReadCollection(cref, false)
 			if err != nil {
 				log.Fatalln(err)
 			}
@@ -62,23 +57,6 @@ func main() {
 	}
 
 	defer db.CloseConn()
-}
-
-func resetUserRecommmendations(db firestoreDB.FirestoreDB, cref string, userId string) error {
-	fmt.Printf("Resetting user <%v> recommendations collection...", userId)
-	err := db.DeleteCollection(cref, 20)
-	return err
-}
-
-func readUserRecommendations(db firestoreDB.FirestoreDB, cref string, printDocuments bool, userId string) error {
-	fmt.Printf("Reading user <%v> recommendations collection...", userId)
-	err := db.ReadCollection(cref, false)
-	return err
-}
-
-func writeUserRecommendationIds(db firestoreDB.FirestoreDB, documentName string, value *domain.Recommendations) error {
-	err := db.SetDocument(documentName, value)
-	return err
 }
 
 func readConfigFile(configFileName string) domain.Config {
